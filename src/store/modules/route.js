7ab849182b4ca486af51f82be4ea6f2a10da532e
Lambert <lambert@zr.slimxu.com>
2021-04-19 15:38:40 +0800

    注释

--- src/store/modules/route.js
+++ src/store/modules/route.js
@@ -1,15 +1,17 @@
 const serviceMap = [
+	// 第一级菜单
 	{
 		id: '1001',
 		title: '系统'
 	},
+	// 第二级菜单
 	{
 		id: '1300',
 		parentId: '1001',
-		path: '/system',
 		icon: 'bb_management_system',
 		title: '系统管理'
 	},
+	// 第三级菜单
 	{
 		id: '1310',
 		parentId: '1300',
