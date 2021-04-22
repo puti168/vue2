2faf4f8031c0c2a0f15ce41c9fb74418ebc0741a
Lambert <lambert@zr.slimxu.com>
2021-04-22 11:56:04 +0800

    会员部分

--- src/store/modules/route.js
+++ src/store/modules/route.js
@@ -27,91 +27,91 @@ const serviceMap = [
 	{
 		id: '4',
 		parentId: '3',
-		path: '/system/systemFunctions/changePassword',
+		path: '/member/memberFunction/memberManagement',
 		title: '会员管理'
 	},
 	{
 		id: '5',
 		parentId: '3',
-		path: '/system/systemFunctions/account',
+		path: '/member/memberFunction/newMember',
 		title: '新增会员配置'
 	},
 	{
 		id: '6',
 		parentId: '3',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/bankCard',
 		title: '银行卡管理'
 	},
 	{
 		id: '7',
 		parentId: '3',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/virtualCurrency',
 		title: '虚拟币账户管理'
 	},
 	{
 		id: '8',
 		parentId: '3',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/memberFeedback',
 		title: '会员反馈记录'
 	},
 	{
 		id: '10',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/withdraw',
 		title: '会员提款审核'
 	},
 	{
 		id: '11',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/recharge',
 		title: '充值补单审核'
 	},
 	{
 		id: '12',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/bankCardReview',
 		title: '银行卡审核'
 	},
 	{
 		id: '13',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/virtualAccount',
 		title: '虚拟币账号审核'
 	},
 	{
 		id: '14',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/amountAdjustment',
 		title: '金额调整审核'
 	},
 	{
 		id: '15',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/addMember',
 		title: '新增会员审核'
 	},
 	{
 		id: '16',
 		parentId: '9',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/dataOpration',
 		title: '资料操作审核'
 	},
 	{
 		id: '18',
 		parentId: '17',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/transactionRecord',
 		title: '会员交易记录'
 	},
 	{
 		id: '19',
 		parentId: '17',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/winlossRecord',
 		title: '会员输赢记录'
 	},
 	{
 		id: '20',
 		parentId: '17',
-		path: '/system/systemFunctions/role',
+		path: '/member/memberFunction/accountChangeRecord',
 		title: '会员金流账变记录'
 	}
 ]
---
+++ src/views/member/memberReview/withdraw/components/editForm.vue
@@ -0,0 +1,64 @@
+<template>
+	<el-form ref="form" :model="editFormData" label-width="80px">
+		<el-form-item label="银行卡号">
+			<el-input
+				v-model="editFormData.bankCode"
+				clearable
+				size="medium"
+				style="width: 280px"
+				placeholder="请输入银行卡号"
+			></el-input>
+		</el-form-item>
+		<el-form-item label="银行名称">
+			<el-input
+				v-model="editFormData.bankName"
+				clearable
+				size="medium"
+				style="width: 280px"
+				placeholder="请输入银行名称"
+			></el-input>
+		</el-form-item>
+		<el-form-item label="时间">
+			<el-date-picker
+				v-model="formTime.time"
+				size="medium"
+				format="yyyy-MM-dd HH:mm:ss"
+				type="datetimerange"
+				range-separator="-"
+				start-placeholder="开始日期"
+				end-placeholder="结束日期"
+				align="right"
+				value-format="yyyy-MM-dd HH:mm:ss"
+				clearable
+				style="width: 280px"
+			></el-date-picker>
+		</el-form-item>
+	</el-form>
+</template>
+
+<script>
+export default {
+	components: {},
+	props: { editFormData: { type: Object, default: () => ({}) } },
+	data() {
+		return {
+			queryData: {},
+			formTime: {
+				time: []
+			}
+		}
+	},
+	computed: {},
+	watch: {
+		editFormData(newQuestion, oldQuestion) {
+			console.log('newQuestion:', newQuestion)
+			this.queryData = newQuestion
+		}
+	},
+	created() {},
+	mounted() {},
+	methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
