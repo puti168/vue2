ce0b05ba2df73af5542f36e8e05e9ca3f733c6cf
Heviis <abcd1234>
2021-04-24 20:27:35 +0800

    黑名单ip UI 接口

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,12 @@ import user from './user'
 import secret from './secret'
 import bankController from './bankController'
 import roleController from './roleController'
+import riskManage from './riskManage'
 
 export default {
 	...bankController,
 	...user,
 	...secret,
-	...roleController
+	...roleController,
+	...riskManage
 }
