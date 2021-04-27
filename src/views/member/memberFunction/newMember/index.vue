c2c04d634a966065fce2da8d34a7ef0f79ce0f68
darcy <darcy.com>
2021-04-27 14:26:37 +0800

    监听滚动

--- src/views/member/memberFunction/newMember/index.vue
+++ src/views/member/memberFunction/newMember/index.vue
@@ -1,302 +1,218 @@
 <template>
-  <div class="game-container report-container">
-    <div class="header flex-h flex-bc">
-      <h2 class="h2-line">门票记录</h2>
-      <div class="head flex-h-end">
-        <el-button
-          type="primary"
-          icon="el-icon-search"
-          :disabled="loading"
-          size="medium"
-          @click="query"
+  <div>
+    <div class="ps">
+      <el-form ref="form" :inline="true" :model="queryData">
+        <el-form-item label="会员账号:">
+          <el-input
+            v-model="queryData.roleName"
+            clearable
+            size="medium"
+            style="width: 280px"
+            placeholder="请输入会员账号"
+            :disabled="loading"
+            @keyup.enter.native="enterSearch"
+          ></el-input>
+        </el-form-item>
+        <el-form-item style="margin-left: 30px">
+          <el-button
+            type="primary"
+            icon="el-icon-search"
+            :disabled="loading"
+            size="medium"
+            @click="query"
+          >
+            查询
+          </el-button>
+          <el-button
+            icon="el-icon-refresh-left"
+            :disabled="loading"
+            size="medium"
+            @click="reset"
+          >
+            重置
+          </el-button>
+        </el-form-item>
+      </el-form>
+      <el-tabs v-model="activeName" class="tabsBox" @tab-click="handleClick">
+        <el-tab-pane label="基本信息" name="first" class="nav-list-item"> </el-tab-pane>
+        <el-tab-pane label="财务信息" name="second" class="nav-list-item"></el-tab-pane>
+        <el-tab-pane label="登录信息" name="third" class="nav-list-item"></el-tab-pane>
+        <el-tab-pane
+          label="银行卡/虚拟币账号信息"
+          name="fourth"
+          class="nav-list-item"
+        ></el-tab-pane>
+      </el-tabs>
+    </div>
+    <!-- <section class="floor-nav" id="floorNavList">
+      <ul class="nav-list">
+        <li
+          class="nav-list-item"
+          v-for="(item, index) in floorList"
+          :key="item.id"
+          @click="setFloorNavMountClick(index)"
         >
-          查询
-        </el-button>
-        <el-button
-          icon="el-icon-refresh-left"
-          :disabled="loading"
-          size="medium"
-          @click="reset"
-        >
-          重置
-        </el-button>
-        <el-button
-          type="primary"
-          icon="el-icon-folder-add"
-          size="medium"
-          @click="add"
-        >
-          新增
-        </el-button>
+          {{ item.name }}
+        </li>
+      </ul>
+    </section> -->
+    <div v-for="item in floorList" :key="item.id" class="floor-item">
+      <div class="floor-item-box">
+        <h3>{{ item.name }}</h3>
       </div>
     </div>
-    <div class="view-container dealer-container">
-      <div class="params">
-        <el-form ref="form" :inline="true" :model="queryData" label-width="100px">
-          <el-form-item label="银行卡号">
-            <el-input
-              v-model="queryData.bankCode"
-              clearable
-              size="medium"
-              style="width: 280px"
-              placeholder="请输入银行卡号"
-              :disabled="loading"
-              @keyup.enter.native="enterSearch"
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="银行名称">
-            <el-input
-              v-model="queryData.bankName"
-              clearable
-              size="medium"
-              style="width: 280px"
-              placeholder="请输入银行名称"
-              :disabled="loading"
-              @keyup.enter.native="enterSearch"
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="时间">
-            <el-date-picker
-              v-model="formTime.time"
-              size="medium"
-              :picker-options="pickerOptions"
-              format="yyyy-MM-dd HH:mm:ss"
-              type="datetimerange"
-              range-separator="-"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期"
-              align="right"
-              clearable
-              value-format="timestamp"
-              style="width: 280px"
-            ></el-date-picker>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div class="content">
-        <el-table
-          v-loading="loading"
-          border
-          size="mini"
-          class="small-size-table"
-          :data="dataList"
-          style="width: 100%"
-          :header-cell-style="getRowClass"
-        >
-          <el-table-column
-            prop="bankCode"
-            align="center"
-            label="银行卡号"
-          ></el-table-column>
-          <el-table-column
-            prop="bankName"
-            align="center"
-            label="银行名称"
-          ></el-table-column>
-          <el-table-column
-            prop="createDt"
-            align="center"
-            label="创建时间"
-          ></el-table-column>
-          <el-table-column
-            prop="updateDt"
-            align="center"
-            label="更新时间"
-          ></el-table-column>
-
-          <el-table-column align="center" label="操作">
-            <template slot-scope="scope">
-              <el-button
-                type="danger"
-                icon="el-icon-delete"
-                size="medium"
-                @click="deleteUp(scope.row)"
-              >
-                删除
-              </el-button>
-              <el-button
-                type="warning"
-                icon="el-icon-edit"
-                size="medium"
-                @click.stop="editUp(scope.row)"
-              >
-                修改
-              </el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-        <!-- 分页 -->
-        <el-pagination
-          v-show="dataList.length > 0"
-          :current-page.sync="pageNum"
-          layout="total, sizes,prev, pager, next, jumper"
-          :page-size="pageSize"
-          :page-sizes="$store.getters.pageSizes"
-          :total="15"
-          @current-change="handleCurrentChange"
-          @size-change="handleSizeChange"
-        ></el-pagination>
-        <el-dialog
-          :title="moduleBox"
-          center
-          :visible.sync="editVisible"
-          :before-close="closeFormDialog"
-          width="410px"
-        >
-          <editForm v-if="moduleBox == '新增银行信息'" ref="addForm"></editForm>
-          <editForm v-else ref="editForm" :editFormData="editFormData"></editForm>
-          <div slot="footer" class="dialog-footer">
-            <el-button @click="editVisible = false">取 消</el-button>
-            <el-button
-              v-if="moduleBox == '新增银行信息'"
-              type="primary"
-              @click="submitAdd"
-              >确 定</el-button>
-            <el-button v-else type="primary" @click="submitEdit">确 定</el-button>
-          </div>
-        </el-dialog>
-      </div>
-    </div>
+    <div class="floor-warpper"></div>
   </div>
 </template>
 
 <script>
 import list from '@/mixins/list'
-import editForm from './components/editForm'
-// import {
-//   getQueryBank,
-//   setAddBank,
-//   setDeleteBank,
-//   setEidteBank,
-// } from "@/api/bankController";
 export default {
-  name: '',
-  components: {
-    editForm
-  },
+  name: 'IFloor',
   mixins: [list],
   data() {
     return {
       queryData: {},
-      formTime: {
-        time: []
+      activeName: 'first',
+      tabList: ['first', 'second', 'third', 'fourth'],
+      element: {
+        nav_item: null,
+        floor_item: null
       },
-      dataList: [],
-      moduleBox: '',
-      showForm: '',
-      editVisible: false,
-      editFormData: {}
+      timer: null,
+      floorList: [
+        { id: 1, name: 'F1' },
+        { id: 2, name: 'F2' },
+        { id: 3, name: 'F3' },
+        { id: 4, name: 'F4' }
+      ]
     }
   },
-  computed: {},
   mounted() {
-    for (let i = 0; i < 10; i++) {
-      this.dataList[i] = {
-        bankCode: '165416416464654',
-        bankName: '中国银行',
-        createDt: '2021-02-13 20:28:54',
-        updateDt: '2021-02-13 20:28:54'
-      }
+    this.element = {
+      nav_item: document.getElementsByClassName('nav-list-item'),
+      floor_item: document.getElementsByClassName('floor-item')
     }
+    this.element.nav_item[0].classList.add('active')
+    this.element.floor_item[0].classList.add('marginT')
+    window.addEventListener('scroll', this.floorSrcollEventListener)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.floorSrcollEventListener)
   },
   methods: {
-    // loadData(params) {
-    //   params = {
-    //     ...this.getParams(params)
-    //   }
-    //   getQueryBank(params).then((res) => {
-    //     console.log('res:', res)
-    //     if (res.code === 200) {
-    //       this.loading = false
-    //       this.dataList = res.data
-    //     } else {
-    //       this.loading = false
-    //       this.$message({
-    //         message: res.msg,
-    //         type: 'error'
-    //       })
-    //     }
-    //   })
-    // },
-    query() {
-      this.loading = true
-      const create = this.formTime.time || []
-      const [startTime, endTime] = create
-      const params = {
-        ...this.queryData,
-        pageNum: 1,
-        startTime: startTime && startTime + '',
-        endTime: endTime && endTime + ''
+    query() {},
+    reset() {},
+    handleClick(tab, event) {
+      console.log(tab.index)
+      this.setFloorNavMountClick(tab.index)
+    },
+    /**
+     * 设置楼层导航事件驱动方法
+     * @param {Number} index  楼层下标
+     */
+    setFloorNavMountClick(index) {
+      const { floor_item } = this.element
+      const floor_offsetTop = floor_item[index].offsetTop - floor_item[0].offsetTop
+      const window_scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop
+      const timer = {
+        step: 60,
+        times: 20,
+        FLOOR_OFFSETTOP: floor_offsetTop
       }
-      console.log(params)
-      this.loadData(params)
+      if (window_scrollTop > floor_offsetTop) {
+        this.setFloorScrollArrowUp(timer)
+      } else if (window_scrollTop === floor_offsetTop) {
+        return false
+      } else {
+        this.setFloorScrollArrowDown(timer)
+      }
     },
-    reset() {
-      this.queryData = {}
-      this.formTime.time = []
-      // this.loadData()
+    /**
+     * 设置楼层向上滚动
+     * @param {Object} timer 定时器配置
+     */
+    setFloorScrollArrowUp(timer) {
+      clearInterval(this.timer)
+      this.timer = setInterval(() => {
+        const window_scrollTop =
+          document.documentElement.scrollTop || document.body.scrollTop
+        if (window_scrollTop <= timer.FLOOR_OFFSETTOP) {
+          document.documentElement.scrollTop = timer.FLOOR_OFFSETTOP
+          document.body.scrollTop = timer.FLOOR_OFFSETTOP
+          clearInterval(this.timer)
+        } else {
+          document.documentElement.scrollTop = window_scrollTop - timer.step
+          document.body.scrollTop = window_scrollTop - timer.step
+        }
+      }, timer.times)
     },
-
-    add() {
-      this.moduleBox = '新增银行信息'
-      this.editVisible = true
+    /**
+     * 设置楼层向下滚动
+     * @param {Object} timer 定时器配置
+     */
+    setFloorScrollArrowDown(timer) {
+      clearInterval(this.timer)
+      this.timer = setInterval(() => {
+        const window_scrollTop =
+          document.documentElement.scrollTop || document.body.scrollTop
+        if (window_scrollTop + 300 >= timer.FLOOR_OFFSETTOP) {
+          document.documentElement.scrollTop = timer.FLOOR_OFFSETTOP
+          document.body.scrollTop = timer.FLOOR_OFFSETTOP
+          clearInterval(this.timer)
+        } else {
+          document.documentElement.scrollTop = window_scrollTop + timer.step
+          document.body.scrollTop = window_scrollTop - timer.step
+        }
+      }, timer.times)
     },
-    submitAdd() {
-      console.log(this.$refs.addForm)
-      //   setAddBank(this.queryData).then((res) => {
-      //     console.log(res);
-      //   });
-    },
-    deleteUp(val) {
-      console.log(val)
-      this.$confirm('确定删除此银行卡号吗?', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      })
-        .then(() => {
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          })
-          // setDeleteBank(val).then((res) => {
-          //   console.log(res);
-          // });
-        })
-        .catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          })
-        })
-    },
-    editUp(val) {
-      this.moduleBox = '修改银行信息'
-      this.editVisible = true
-      this.editFormData = val
-    },
-    submitEdit() {
-      // setEidteBank().then((res) => {
-      //   console.log(res);
-      // });
-    },
-    handleCurrentChange() {
-      this.loadData()
-    },
-    closeFormDialog() {
-      this.editVisible = false
-    },
-    enterSubmit() {
-      this.query()
+    /**
+     * 监听窗口滚动楼层导航动态定位
+     */
+    floorSrcollEventListener() {
+      const { nav_item, floor_item } = this.element
+      const window_scrollTop =
+        document.documentElement.scrollTop + 180 || document.body.scrollTop + 180
+      for (let i = 0, len = floor_item.length; i < len; i++) {
+        const floor_offsetTop = floor_item[i].offsetTop - floor_item[0].offsetTop
+        if (window_scrollTop >= floor_offsetTop) {
+          for (let n = 0, len = nav_item.length; n < len; n++) {
+            console.log(i, this.tabList[i])
+            i === n
+              ? (this.activeName = this.tabList[i])
+              : (this.activeName = this.tabList[i])
+          }
+        }
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-/deep/.el-dialog__header {
-  text-align: center;
-  color: #909399;
-  font-weight: 700;
+.ps {
+  width: 100%;
+  height: 110px;
+  position: fixed;
+  top: 104px;
+  padding: 15px;
+  background: #fff;
+}
+/deep/ .tabsBox .el-tabs__nav-wrap::after {
+  display: none;
+}
+/deep/ .tabsBox .el-tabs__content {
+  display: none;
+}
+// .floor-item {
+//   width: 100%;
+//   min-height: 500px;
+//   text-align: center;
+//   color: #fff;
+//   background-color: #404040;
+// }
+.marginT {
+  margin-top: 110px;
 }
 </style>
