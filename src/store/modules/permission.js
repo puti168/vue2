dc52da0ecfb5373a8b161ea8550520dbc6b752e4
Lambert <lambert@zr.slimxu.com>
2021-05-08 22:38:09 +0800

    创建编辑

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -196,6 +196,19 @@ const actions = {
 						}
 					})
 				}
+				if (item.name === '游戏') {
+					item.children.forEach(data => {
+						if (data.name === '游戏配置') {
+							data.children.push({
+								path: '/game/gameConfig/gameManagementEdit',
+								name: 'gameManagementEdit',
+								component: () => import(`@/views/game/gameConfig/gameManagementEdit/index`),
+								meta: { title: '游戏管理创建编辑' },
+								hidden: true
+							})
+						}
+					})
+				}
 			})
 			parentRoutes = parentRoutes.concat(rootRoutes)
 			commit('SET_ROUTES', {
