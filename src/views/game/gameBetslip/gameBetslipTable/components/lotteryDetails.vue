bfaf8411c06abb0bd409c42006a68b9a4b01a9a7
darcy <darcy.com>
2021-05-18 00:33:58 +0800

    游戏注单   七大类游戏详情联调

--- src/views/game/gameBetslip/gameBetslipTable/components/lotteryDetails.vue
+++ src/views/game/gameBetslip/gameBetslipTable/components/lotteryDetails.vue
@@ -1,75 +1,102 @@
 <template>
-    <div class="dealer-container">
-      <div class="betslip">
-        <strong class="strong">投注人信息</strong>
-        <div>
-          <el-row class="paddingLR paddingB">
-            <el-col :span="6">注单号： 45646544445451</el-col>
-            <el-col :span="6">三方注单号： 45646544445451</el-col>
-            <el-col :span="6">游戏平台： B端名称</el-col>
-            <el-col :span="6">彩种名称： 极速飞艇</el-col>
-            <el-col :span="6">注单状态： 已结算</el-col>
-            <el-col :span="6">期号： 4545454545454A4</el-col>
-            <el-col :span="6">模式： 2元</el-col>
-            <el-col :span="6">赔率： 8.11</el-col>
-            <el-col :span="6">投注内容： 5，6，8，9，10</el-col>
-            <el-col :span="6">判奖内容： 5，6，8，9，10</el-col>
-            <el-col :span="6">输赢金额： -60.00</el-col>
-            <el-col :span="6">终端： H5</el-col>
-            <el-col :span="6">投注IP： 192.168.0.1</el-col>
-            <el-col :span="6">投注时间： 1991/11/11 06:11:12</el-col>
-            <el-col :span="6">结算时间： 1991/11/11 07:11:12</el-col>
-            <el-col :span="6">同步时间： 1991/11/11 08:11:12</el-col>
-          </el-row>
-        </div>
-        <el-table
-          border
-          size="mini"
-          class="small-size-table"
-          :data="tableData"
-          style="width: 100%"
-          :header-row-style="{ height: '24px', lineHeight: '24px', background: '#000' }"
-        >
-          <el-table-column prop="vipSerialNum" label="玩法名称" align="center">
-          </el-table-column>
-          <el-table-column prop="vipSerialNum" label="注数" align="center">
-          </el-table-column>
-          <el-table-column prop="vipSerialNum" label="倍数" align="center">
-          </el-table-column>
-          <el-table-column prop="vipSerialNum" label="投注金额" align="center">
-          </el-table-column>
-          <el-table-column prop="vipSerialNum" label="状态" align="center">
-          </el-table-column>
-        </el-table>
+  <div class="dealer-container">
+    <div class="betslip">
+      <strong class="strong">注单信息</strong>
+      <div>
+        <el-row class="paddingLR paddingB">
+          <el-col :span="6">注单号： {{ dataList.id }}</el-col>
+          <el-col :span="6">三方注单号： {{ dataList.thirdOrderId }}</el-col>
+          <el-col
+:span="6"
+>游戏平台：
+            <span v-for="item in gameTypeList" :key="item.gameCode">
+              {{ dataList.gameCode === item.gameCode ? item.gameName : "" }}
+            </span>
+          </el-col>
+          <el-col :span="6">彩种名称： {{ dataList.seriesName }}</el-col>
+          <el-col :span="6">注单状态： {{ dataList.betStatus }}</el-col>
+          <el-col :span="6">期号： {{ dataList.ticketPlanNo }}</el-col>
+          <el-col :span="6">模式： {{ dataList.betModel }}</el-col>
+          <el-col :span="6">赔率： {{ dataList.oddsValue }}</el-col>
+          <el-col :span="6">
+            <span> 投注内容：</span>
+            <span> {{ dataList.betNum }}</span>
+          </el-col>
+          <el-col :span="6">
+            判奖内容： <span>{{ dataList.judgeResultCn }}</span>
+          </el-col>
+          <el-col :span="6">输赢金额： {{ dataList.netAmount }}</el-col>
+          <el-col :span="6">
+            终端： {{ typeFilter(dataList.deviceType, "betDeviceType") }}
+          </el-col>
+          <el-col :span="6">投注IP： {{ dataList.loginIp }}</el-col>
+          <el-col :span="6">投注时间： {{ dataList.createAt }}</el-col>
+          <el-col :span="6">结算时间： {{ dataList.netAt }}</el-col>
+          <el-col :span="6">同步时间： {{ dataList.synchronizationTime }}</el-col>
+        </el-row>
       </div>
+      <el-table
+        border
+        size="mini"
+        class="small-size-table"
+        :data="tableData"
+        style="width: 100%"
+        :header-row-style="{ height: '24px', lineHeight: '24px', background: '#000' }"
+      >
+        <el-table-column prop="playName" label="玩法名称" align="center">
+        </el-table-column>
+        <el-table-column prop="betCount" label="注数" align="center"> </el-table-column>
+        <el-table-column prop="betMultiple" label="倍数" align="center">
+        </el-table-column>
+        <el-table-column prop="betAmount" label="投注金额" align="center">
+        </el-table-column>
+        <el-table-column prop="betStatus" label="状态" align="center"> </el-table-column>
+      </el-table>
     </div>
+  </div>
 </template>
 
 <script>
+import list from '@/mixins/list'
 export default {
   name: 'CpDetails',
   components: {},
-  props: {},
+  mixins: [list],
+  props: {
+    dataList: {
+      typeL: Object,
+      default: () => ({})
+    }
+  },
   data() {
     return {
+      gameTypeList: {},
       tableData: []
     }
   },
   computed: {},
   watch: {},
-  created() {},
-  mounted() {
-    for (let i = 0; i < 10; i++) {
-      this.tableData[i] = {
-        bankCode: '165416416464654',
-        bankName: '中国银行',
-        createDt: '2021-02-13 20:28:54',
-        updateDt: '2021-02-13 20:28:54',
-        index: i
-      }
-    }
+  created() {
+    this.getGameTypeList()
   },
-  methods: {}
+  mounted() {
+    const res = {}
+    res.playName = this.dataList.playName
+    res.betCount = this.dataList.betCount
+    res.betMultiple = this.dataList.betMultiple
+    res.betAmount = this.dataList.betAmount
+    res.betStatus = this.dataList.betStatus
+    this.tableData.push(res)
+  },
+  methods: {
+    getGameTypeList() {
+      this.$api.getMerchantGameGamePlant().then((res) => {
+        if (res.code === 200) {
+          this.gameTypeList = res.data
+        }
+      })
+    }
+  }
 }
 </script>
 
