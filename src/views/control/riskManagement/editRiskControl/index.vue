e4e595e74dc83d86a717e43c3463756673ada56c
Heviis <abcd1234>
2021-05-26 13:15:38 +0800

    编辑风控层级UI

--- src/views/control/riskManagement/editRiskControl/index.vue
+++ src/views/control/riskManagement/editRiskControl/index.vue
@@ -1,310 +1,277 @@
 <template>
-  <div class="game-container report-container">
-    <div class="view-container dealer-container">
-        <h1>编辑风控层级</h1>
-      <div class="params">
-        <el-form ref="form" :inline="true" :model="queryData">
-          <el-form-item label="登录时间:">
-            <el-date-picker
-              v-model="loginTime"
-              size="medium"
-              :picker-options="pickerOptions"
-              format="yyyy-MM-dd HH:mm:ss"
-              type="datetimerange"
-              range-separator="-"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期"
-              align="right"
-              :clearable="false"
-              :default-time="defaultTime"
-              style="width: 375px"
-            ></el-date-picker>
-          </el-form-item>
-          <el-form-item label="代理账号:" prop="userName">
-            <el-input
-              v-model="queryData.userName"
-              clearable
-              :maxlength="11"
-              size="medium"
-              style="width: 180px; margin-right: 20px"
-              placeholder="请输入"
-              :disabled="loading"
-              @keyup.enter.native="enterSearch"
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="代理类型:" class="tagheight" prop="accountType">
-            <el-select
-              v-model="queryData.accountType"
-              style="width: 280px"
-              multiple
-              placeholder="默认选择全部"
-              :popper-append-to-body="false"
-            >
-              <el-option
-                v-for="item in accountType"
-                :key="item.code"
-                :label="item.description"
-                :value="item.code"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="登录状态:" class="tagheight" prop="loginStatus">
-            <el-select
-              v-model="queryData.loginStatus"
-              style="width: 280px"
-              clearable
-              placeholder="默认选择全部"
-              :popper-append-to-body="false"
-            >
-              <el-option
-                v-for="item in loginStatusType"
-                :key="item.description"
-                :label="item.description"
-                :value="item.code"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="登录IP:" prop="loginIp">
-            <el-input
-              v-model="queryData.loginIp"
-              clearable
-              :maxlength="15"
-              size="medium"
-              style="width: 180px"
-              placeholder="请输入内容"
-              :disabled="loading"
-              @keyup.enter.native="enterSearch"
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="IP归属地:" prop="ipAttribution">
-            <el-input
-              v-model="queryData.ipAttribution"
-              clearable
-              :maxlength="10"
-              size="medium"
-              style="width: 180px"
-              placeholder="请输入内容"
-              :disabled="loading"
-              @keyup.enter.native="enterSearch"
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="登录终端:" class="tagheight" prop="deviceType">
-            <el-select
-              v-model="queryData.deviceType"
-              style="width: 280px"
-              multiple
-              placeholder="默认选择全部"
-              :popper-append-to-body="false"
-            >
-              <el-option
-                v-for="item in loginDeviceType"
-                :key="item.code"
-                :label="item.description"
-                :value="item.code"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="终端设备号:" prop="deviceNo">
-            <el-input
-              v-model="queryData.deviceNo"
-              clearable
-              :maxlength="50"
-              size="medium"
-              style="width: 180px"
-              placeholder="请输入内容"
-              :disabled="loading"
-              @keyup.enter.native="enterSearch"
-            ></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              type="primary"
-              icon="el-icon-search"
-              :disabled="loading"
-              size="medium"
-              @click="search"
-            >
-              查询
-            </el-button>
-            <el-button
-              icon="el-icon-refresh-left"
-              :disabled="loading"
-              size="medium"
-              @click="reset"
-            >
-              重置
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div class="content">
-        <el-table
-          v-loading="loading"
-          border
-          size="mini"
-          class="small-size-table"
-          :data="tableData"
-          style="width: 100%"
-          :header-cell-style="getRowClass"
-          @sort-change="_changeTableSort"
-        >
-          <el-table-column
-            prop="loginTime"
-            align="center"
-            label="登录时间"
-            sortable="custom"
-          ></el-table-column>
-          <el-table-column prop="loginStatus" align="center" label="登录状态">
-            <template slot-scope="scope">
-              {{ typeFilter(scope.row.loginStatus, "loginStatusType") }}
-            </template>
-          </el-table-column>
-          <el-table-column prop="userName" align="center" label="代理账号">
-            <template slot-scope="scope">
-              <Copy v-if="!!scope.row.userName" :title="scope.row.userName" :copy="copy">
-                {{ scope.row.userName }}
-              </Copy>
-              <span v-else>-</span>
-            </template>
-          </el-table-column>
-          <el-table-column prop="accountType" align="center" label="代理类型">
-            <template slot-scope="scope">
-              {{ typeFilter(scope.row.accountType, "accountType") }}
-            </template>
-          </el-table-column>
-          <el-table-column prop="loginIp" align="center" label="登录IP"></el-table-column>
-          <el-table-column
-            prop="ipAttribution"
-            align="center"
-            label="IP归属地"
-          ></el-table-column>
-          <el-table-column prop="deviceType" align="center" label="登录终端">
-            <template slot-scope="scope">
-              {{ typeFilter(scope.row.deviceType, "loginDeviceType") }}
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="deviceNo"
-            align="center"
-            label="终端设备号"
-          ></el-table-column>
-          <el-table-column
-            prop="loginReference"
-            align="center"
-            label="登录地址"
-          ></el-table-column>
-          <el-table-column
-            prop="browseContent"
-            align="center"
-            label="设备版本"
-          ></el-table-column>
-          <el-table-column
-            prop="loginError"
-            align="center"
-            label="备注"
-          ></el-table-column>
-        </el-table>
-        <!-- 分页 -->
-        <el-pagination
-          :current-page.sync="pageNum"
-          class="pageValue"
-          background
-          layout="total, sizes,prev, pager, next, jumper"
-          :page-size="pageSize"
-          :page-sizes="pageSizes"
-          :total="total"
-          @current-change="handleCurrentChange"
-          @size-change="handleSizeChange"
-        ></el-pagination>
-      </div>
+    <div class="game-container report-container addAgent-container">
+        <div class="line-member"></div>
+        <div class="addAgent-content">
+            <div class="form-header">
+                <span>编辑风控层级</span>
+                <span>
+					<code style="color:#FF3B30;">*</code>
+					为必填项
+				</span>
+            </div>
+            <el-form ref="form" :model="form" :rules="rules" label-width="100px">
+                <el-form-item label="风控类型:" prop="accountType">
+                    <el-select
+                        v-model="form.accountType"
+                        size="medium"
+                        placeholder="全部"
+                        clearable
+                        style="width: 365px"
+                    >
+                        <el-option
+                            v-for="item in accountTypeArr"
+                            :key="item.code"
+                            :label="item.description"
+                            :value="item.code"
+                        ></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="风控账号:" prop="userName">
+                    <el-input
+                        v-model="form.userName"
+                        size="medium"
+                        maxlength="11"
+                        placeholder="4-11位，最少2个字母+数字组合，首位字母"
+                        clearable
+                        style="width: 365px"
+                    ></el-input>
+                    <el-button
+                        type="primary"
+                        style="margin-left: 20px"
+                    >
+                        <i v-show="loading" class="el-icon-loading"></i>
+                        查询
+                    </el-button>
+                </el-form-item>
+                <el-form-item label="风控层级:" prop="password">
+                    <el-input
+                        v-model="form.password"
+                        size="medium"
+                        placeholder="8-12位，字母+数字组合"
+                        clearable
+                        oninput="value=value.replace(/(^\s*)|(\s*$)/g ,'')"
+                        maxlength="12"
+                        style="width: 365px"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item label="审核信息:">
+                    <el-input
+                        v-model="form.applyInfo"
+                        size="medium"
+                        type="textarea"
+                        placeholder="请输入"
+                        clearable
+                        maxlength="50"
+                        style="width: 365px"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button
+                        type="primary"
+                        icon="el-icon-search"
+                        :disabled="loading"
+                        size="medium"
+                        @click="add"
+                    >
+                        提交
+                    </el-button>
+                    <el-button
+                        icon="el-icon-refresh-left"
+                        :disabled="loading"
+                        size="medium"
+                        @click="reset"
+                    >
+                        重置
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import list from '@/mixins/list'
-import dayjs from 'dayjs'
 import { routerNames } from '@/utils/consts'
-const startTime = dayjs().startOf('day').valueOf()
-const endTime = dayjs().endOf('day').valueOf()
-
+import list from '@/mixins/list'
+import { notSpecial2, isHaveEmoji } from '@/utils/validate'
 export default {
-  name: routerNames.editRiskControl,
-  components: {},
-  mixins: [list],
-  data() {
-    return {
-      queryData: {},
-      loginTime: [startTime, endTime],
-      now: dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss'),
-      tableData: []
+    name: routerNames.addAgent,
+    mixins: [list],
+    data() {
+        return {
+            loading: false,
+            form: {
+                accountType: '4',
+                userName: '',
+                password: '',
+                applyInfo: ''
+            }
+        }
+    },
+    computed: {
+        accountTypeArr() {
+            return this.globalDics.accountType
+        },
+        rules() {
+            const reg1 = /^[A-Za-z]{1}(?=(.*[a-zA-Z]){1,})(?=(.*[0-9]){1,})[0-9A-Za-z]{3,10}$/
+            const reg2 = /(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,12}/
+
+            const testUserName = (rule, value, callback) => {
+                const isSpecial = !notSpecial2(String(value))
+                const isRmoji = isHaveEmoji(String(value))
+                if (isSpecial) {
+                    callback(new Error('不支持空格及特殊字符'))
+                } else if (isRmoji) {
+                    callback(new Error('不支持表情'))
+                } else if (!reg1.test(value)) {
+                    callback(new Error('请输入4-11位，最少2个字母+数字组合，首位字母'))
+                } else {
+                    callback()
+                }
+            }
+
+            const testPassword = (rule, value, callback) => {
+                const isSpecial = !notSpecial2(String(value))
+                const isRmoji = isHaveEmoji(String(value))
+                if (isSpecial) {
+                    callback(new Error('不支持空格及特殊字符'))
+                } else if (isRmoji) {
+                    callback(new Error('不支持表情'))
+                } else if (!reg2.test(value)) {
+                    callback(new Error('请输入8-12位，字母+数字组合'))
+                } else {
+                    callback()
+                }
+            }
+
+            return {
+                accountType: [
+                    { required: true, message: '请选择账号类型', trigger: 'change' }
+                ],
+                userName: [
+                    {
+                        required: true,
+                        validator: testUserName,
+                        trigger: 'blur'
+                    }
+                ],
+                password: [
+                    {
+                        required: true,
+                        validator: testPassword,
+                        trigger: 'blur'
+                    }
+                ]
+            }
+        }
+    },
+    mounted() {},
+    methods: {
+        add() {
+            this.loading = true
+            const params = {
+                ...this.form
+            }
+            let lock = true
+            this.$refs['form'].validate((valid) => {
+                if (valid && lock) {
+                    lock = false
+                    this.$api
+                        .addAgentAPI(params)
+                        .then((res) => {
+                            this.loading = false
+                            lock = true
+                            const { code, data, msg } = res
+                            if (code === 200) {
+                                this.$confirm(`代理${data}资料提交成功`, {
+                                    confirmButtonText: '确定',
+                                    type: 'success',
+                                    showCancelButton: false
+                                })
+                                this.reset()
+                            } else {
+                                this.$message({
+                                    message: msg,
+                                    type: 'error'
+                                })
+                            }
+                        })
+                        .catch(() => {
+                            this.loading = false
+                            lock = true
+                        })
+                }
+            })
+
+            setTimeout(() => {
+                this.loading = false
+                lock = true
+            }, 1000)
+        },
+        reset() {
+            this.$refs['form'].resetFields()
+            this.form = {
+                accountType: '4',
+                userName: '',
+                password: '',
+                applyInfo: ''
+            }
+        },
+        checkValue(val) {}
     }
-  },
-  computed: {
-    loginDeviceType() {
-      return this.globalDics.loginDeviceType
-    },
-    loginStatusType() {
-      return this.globalDics.loginStatusType
-    },
-    accountType() {
-      return this.globalDics.accountType
-    }
-  },
-  mounted() {},
-  methods: {
-    loadData() {
-      this.loading = true
-      const create = this.loginTime || []
-      const [startTime, endTime] = create
-      let params = {
-        ...this.queryData,
-        loginStartTime: startTime ? dayjs(startTime).format('YYYY-MM-DD HH:mm:ss') : '',
-        loginEndTime: endTime ? dayjs(endTime).format('YYYY-MM-DD HH:mm:ss') : ''
-      }
-      params = {
-        ...this.getParams(params)
-      }
-      this.$api
-        .getProxyDetailProxyLoginLog(params)
-        .then((res) => {
-          this.now = dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss')
-          if (res.code === 200) {
-            this.tableData = res.data.record
-            this.total = res.data.totalRecord
-            this.summary = res.data.summary !== null ? res.data.summary : {}
-          }
-          this.loading = false
-        })
-        .catch(() => {
-          this.loading = false
-        })
-    },
-    reset() {
-      this.queryData = {}
-    },
-    _changeTableSort({ column, prop, order }) {
-      if (prop === 'loginTime') {
-        prop = 1
-      }
-      this.queryData.orderKey = prop
-      if (order === 'ascending') {
-        // 升序
-        this.queryData.orderType = 'asc'
-      } else if (column.order === 'descending') {
-        // 降序
-        this.queryData.orderType = 'desc'
-      }
-      this.loadData()
-    }
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 /deep/.el-dialog__header {
-  text-align: center;
-  color: #909399;
-  font-weight: 700;
+    text-align: center;
+    color: #909399;
+    font-weight: 700;
+}
+.addAgent-container {
+    background-color: #f5f5f5;
+    margin: 0;
+    min-height: calc(100vh - 105px);
+    .line-member {
+        height: 50px;
+    }
+    .addAgent-content {
+        width: 1000px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fff;
+        position: relative;
+        padding-top: 65px;
+        padding-bottom: 65px;
+        .form-header {
+            height: 45px;
+            line-height: 45px;
+            span:nth-child(1) {
+                position: absolute;
+                left: 30px;
+                top: 0;
+                color: #666;
+                font-size: 14px;
+                font-weight: 700;
+            }
+            span:nth-child(2) {
+                position: absolute;
+                right: 30px;
+                color: #999;
+                font-weight: 400;
+                font-size: 14px;
+                top: 0;
+            }
+        }
+    }
+    .addAgent-content::after {
+        position: absolute;
+        top: 45px;
+        content: '';
+        width: 100%;
+        background-color: rgba(233, 233, 233, 1);
+        height: 1px;
+    }
 }
 </style>
