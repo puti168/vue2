e2d9eb95d673584b54db31cedad0b3b4b9df5fa6
Heviis <abcd1234>
2021-06-01 13:54:57 +0800

    OBQJTEST-439【游戏搜索管理】保存，弹出系统异常，删除无请求，导致无法删除

--- src/views/game/gameSearch/gameSearchManage/index.vue
+++ src/views/game/gameSearch/gameSearchManage/index.vue
@@ -105,7 +105,7 @@
 							align="center"
 							label="创建时间"
 							width="180px"
-                            sortable="custom"
+							sortable="custom"
 							:formatter="dateFormat"
 						></el-table-column>
 						<el-table-column prop="updatedBy" align="center" label="最新操作人">
@@ -121,7 +121,7 @@
 							align="center"
 							label="最新操作时间"
 							width="180px"
-                            sortable="custom"
+							sortable="custom"
 							:formatter="dateFormat"
 						></el-table-column>
 						<el-table-column align="center" label="操作">
@@ -275,10 +275,11 @@ export default {
 				this.dataList.map((item) => {
 					return {
 						displayOrder: item.displayOrder,
-						searchInfo: item.searchInfo
+						searchInfo: item.searchInfo,
+						createdAt: item.item,
+						createdBy: item.createdBy
 					}
 				}) || []
-			console.log('createObSearchConfigReqList', createObSearchConfigReqList)
 			const { historySearchGameLimit, hotSearchGameLimit } = this.queryData
 			const params = {
 				createObSearchConfigReqList,
@@ -436,23 +437,23 @@ export default {
 			// })
 		},
 
-        _changeTableSort({ column, prop, order }) {
-            if (prop === 'createdAt') {
-                prop = 1
-            }
-            if (prop === 'updatedAt') {
-                prop = 2
-            }
-            this.queryData.orderKey = prop
-            if (order === 'ascending') {
-                // 升序
-                this.queryData.orderType = 'asc'
-            } else if (column.order === 'descending') {
-                // 降序
-                this.queryData.orderType = 'desc'
-            }
-            this.loadData()
-        }
+		_changeTableSort({ column, prop, order }) {
+			if (prop === 'createdAt') {
+				prop = 1
+			}
+			if (prop === 'updatedAt') {
+				prop = 2
+			}
+			this.queryData.orderKey = prop
+			if (order === 'ascending') {
+				// 升序
+				this.queryData.orderType = 'asc'
+			} else if (column.order === 'descending') {
+				// 降序
+				this.queryData.orderType = 'desc'
+			}
+			this.loadData()
+		}
 	}
 }
 </script>
