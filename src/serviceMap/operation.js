39399b614b6c83c9df5831e231cdc43c22d0bc77
lotus <123>
2021-06-10 19:38:57 +0800

    佣金审核记录，代理加额记录

--- src/serviceMap/operation.js
+++ src/serviceMap/operation.js
@@ -14,7 +14,7 @@ const operation = [
 	{
 		id: '60',
 		parentId: '58',
-		icon: 'bb_homelist',
+		icon: 'bb_operationManage',
 		title: '场馆配置管理'
 	},
 	{
@@ -22,6 +22,12 @@ const operation = [
 		parentId: '58',
 		icon: 'bb_limitManage',
 		title: '信息配置管理'
+	},
+	{
+		id: '62',
+		parentId: '58',
+		icon: 'bb_systemmanagement',
+		title: '活动配置管理'
 	},
 	// 第三级菜单
 	{
@@ -94,6 +100,36 @@ const operation = [
 		id: '74',
 		parentId: '61',
 		path: '/operation/infoConfig/infoLog',
+		title: '操作记录'
+	},
+	{
+		id: '195',
+		parentId: '62',
+		path: '/operation/activityConfig/activityType',
+		title: '活动类型名称'
+	},
+	{
+		id: '196',
+		parentId: '62',
+		path: '/operation/activityConfig/createPage',
+		title: '创建优惠类型页签'
+	},
+	{
+		id: '197',
+		parentId: '62',
+		path: '/operation/activityConfig/activityConfig',
+		title: '优惠活动配置'
+	},
+	{
+		id: '198',
+		parentId: '62',
+		path: '/operation/activityConfig/vipConfig',
+		title: '赞助/vip活动配置'
+	},
+	{
+		id: '199',
+		parentId: '62',
+		path: '/operation/activityConfig/activityConfigLog',
 		title: '操作记录'
 	}
 ]
---
+++ src/views/funds/fundAuditRecord/commissionReviewRecord/components/detail.vue
@@ -0,0 +1,482 @@
+<template>
+	<div class="review-content">
+		<div class="head">
+			<span class="title">佣金审核详情</span>
+			<div v-if="type" class="right-btn">
+				<el-button plain @click="goBack">取消</el-button>
+				<el-button type="success" @click="confirm(true)">一审通过</el-button>
+				<el-button type="danger" @click="confirm(false)">一审拒绝</el-button>
+			</div>
+			<div v-else class="right-btn">
+				<el-button plain @click="goBack">返回</el-button>
+			</div>
+		</div>
+		<div class="main-content">
+			<div class="review-content">
+				<p class="name">代理注册信息</p>
+				<div class="review-flex">
+					<el-table
+						border
+						size="mini"
+						:data="[1]"
+						style="width: 100%"
+						:header-cell-style="getRowClass"
+					>
+						<el-table-column align="center" label="注册时间">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="最后登录时间">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="注册端">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="注册IP">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="注册端设备编号">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="账号类型">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="注册域名">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+					</el-table>
+				</div>
+			</div>
+			<div class="review-content">
+				<p class="name">代理账号信息</p>
+				<div class="review-flex">
+					<el-table
+						border
+						size="mini"
+						:data="[1]"
+						style="width: 100%"
+						:header-cell-style="getRowClass"
+					>
+						<el-table-column align="center" label="代理账号">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="代理姓名">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="账号状态">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="代理标签">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="代理等级">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="绑定银行卡数量">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="虚拟币账号数量">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="累计提款次数">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="累计提款总额（万元）">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+					</el-table>
+				</div>
+			</div>
+			<div class="review-content">
+				<p class="name">账号风控层级</p>
+				<div class="review-flex">
+					<el-table
+						border
+						size="mini"
+						:data="[1]"
+						style="width: 100%"
+						:header-cell-style="getRowClass"
+					>
+						<el-table-column align="center" label="风险代理">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="风险银行卡">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="风险虚拟币">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="风险IP">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="风险终端设备号">
+							<template>
+								{{ list.beforeModify ? list.beforeModify : '-' }}
+							</template>
+						</el-table-column>
+					</el-table>
+				</div>
+			</div>
+			<div class="review-content">
+				<p class="name">佣金账目审核</p>
+				<div class="review-flex">
+					<table class="review-table">
+						<tbody>
+							<tr>
+								<td class="td-title">日期</td>
+								<td>33333333</td>
+								<td class="td-title">佣金比例</td>
+								<td colspan="2">PC</td>
+								<td class="td-title" colspan="2">佣金金额</td>
+								<td colspan="2">33333333333</td>
+							</tr>
+							<tr>
+								<td class="td-title">活跃下级</td>
+								<td>33333333</td>
+								<td class="td-title">有效活跃下级</td>
+								<td>PC</td>
+								<td class="td-title">新增活跃下级</td>
+								<td>33333333333</td>
+								<td class="td-title">新增有效活跃下级</td>
+								<td>否</td>
+								<td class="td-title">总输赢</td>
+								<td>33333333</td>
+							</tr>
+							<tr>
+
+								<td class="td-title">场馆费</td>
+								<td>PC</td>
+								<td class="td-title">总优惠</td>
+								<td>33333333333</td>
+								<td class="td-title">总返水</td>
+								<td>否</td>
+								<td class="td-title">人工加减额</td>
+								<td>否</td>
+								<td class="td-title">补单输赢</td>
+								<td>否</td>
+							</tr>
+							<tr>
+								<td class="td-title">净输赢</td>
+								<td>PC</td>
+								<td class="td-title">上月结余</td>
+								<td>33333333333</td>
+								<td class="td-title">冲正后净输赢</td>
+								<td>否</td>
+								<td class="td-title">等级专享</td>
+								<td>否</td>
+								<td class="td-title">佣金调整</td>
+								<td>否</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="review-content">
+				<p class="name">审核信息</p>
+				<div class="review-flex">
+					<div>一审人: 111</div>
+					<div>一审时间: 222</div>
+					<div>一审备注: 3333</div>
+				</div>
+				<div class="review-flex">
+					<div>二审人: 111</div>
+					<div>二审时间: 222</div>
+					<div>二审备注: 3333</div>
+				</div>
+			</div>
+		</div>
+		<el-dialog
+			title="提交确认"
+			center
+			:visible.sync="visible"
+			:before-close="closeFormDialog"
+			width="610px"
+			class="audit-confirm"
+		>
+			<el-form ref="form" :model="form" :rules="formRules">
+				<el-form-item v-if="action" label="提交审核信息">
+					<el-input
+						v-model="form.remark"
+						clearable
+						type="textarea"
+						:maxlength="50"
+						show-word-limit
+						:autosize="{ minRows: 4, maxRows: 4 }"
+						style="width: 380px"
+						placeholder="请输入"
+					></el-input>
+				</el-form-item>
+				<el-form-item v-else label="提交审核信息" prop="remark">
+					<el-input
+						v-model="form.remark"
+						clearable
+						type="textarea"
+						:maxlength="50"
+						show-word-limit
+						:autosize="{ minRows: 4, maxRows: 4 }"
+						style="width: 380px"
+						placeholder="请输入"
+					></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click="visible = false">取 消</el-button>
+				<el-button type="primary" @click="auditOne">
+					提交
+				</el-button>
+			</div>
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+import list from '@/mixins/list'
+// import dayjs from 'dayjs'
+export default {
+	components: {},
+	mixins: [list],
+	props: {
+		// 审核 true 仅返回 false
+		type: Boolean,
+		rowData: {
+			type: Object,
+			default: () => {}
+		}
+	},
+	data() {
+		return {
+			list: {},
+			form: {
+				remark: ''
+			},
+			visible: false,
+			action: false
+		}
+	},
+	computed: {
+		formRules() {
+			return {
+				remark: [
+					{ required: true, message: '审核拒绝备注必填', trigger: 'blur' }
+				]
+			}
+		}
+	},
+	created() {
+		this.getInfo()
+	},
+	mounted() {},
+	methods: {
+		closeFormDialog() {
+			this.visible = false
+		},
+		confirm(action) {
+			this.remark = ''
+			this.action = action
+			this.visible = true
+		},
+		auditOne() {
+			if (this.action) {
+				const loading = this.$loading({
+					lock: true,
+					text: 'Loading',
+					spinner: 'el-icon-loading',
+					background: 'rgba(0, 0, 0, 0.7)'
+				})
+				const params = {
+					id: this.rowData.id,
+					userId: this.rowData.userId,
+					auditRemark: this.form.remark,
+					auditStatus: this.action ? 2 : 3
+				}
+
+				this.$api
+					.updateProxyAuditRecord(params)
+					.then((res) => {
+						loading.close()
+						if (res.code === 200) {
+							this.$message({
+								type: 'success',
+								message: '操作成功!'
+							})
+							this.visible = false
+							this.goBack()
+						} else {
+							this.$message({
+								message: res.msg,
+								type: 'error'
+							})
+						}
+					})
+					.catch(() => {
+						loading.close()
+					})
+			} else {
+				this.$refs.form.validate((valid) => {
+					if (valid) {
+						const loading = this.$loading({
+							lock: true,
+							text: 'Loading',
+							spinner: 'el-icon-loading',
+							background: 'rgba(0, 0, 0, 0.7)'
+						})
+						const params = {
+							id: this.rowData.id,
+							auditRemark: this.form.remark,
+							auditStatus: this.action ? 2 : 3
+						}
+
+						this.$api
+							.updateProxyAuditRecord(params)
+							.then((res) => {
+								loading.close()
+								if (res.code === 200) {
+									this.$message({
+										type: 'success',
+										message: '操作成功!'
+									})
+									this.visible = false
+									this.goBack()
+								} else {
+									this.$message({
+										message: res.msg,
+										type: 'error'
+									})
+								}
+							})
+							.catch(() => {
+								loading.close()
+							})
+					}
+				})
+			}
+		},
+		goBack() {
+			this.$emit('goBack')
+		},
+		getInfo() {
+			const params = {
+				id: this.rowData.id
+			}
+			this.$api.proxyDetail(params).then((res) => {
+				if (res.code === 200) {
+					const response = res.data
+					this.loading = false
+					this.list = response
+				} else {
+					this.loading = false
+					this.$message({
+						message: res.msg,
+						type: 'error'
+					})
+				}
+			})
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/.el-dialog__header {
+	text-align: center;
+	color: #909399;
+	font-weight: 700;
+}
+.review-content {
+	width: 100%;
+	color: #666666;
+	.review-table {
+		width: 100%;
+		text-align: center; /*文本居中*/
+		border-collapse: collapse; /*表格的边框合并，如果相邻，则共用一个边框*/
+		border-spacing: 0;
+		border: 1px solid #bcbec2;
+		td {
+			border-right: 1px solid #bcbec2;
+			border-bottom: 1px solid #bcbec2;
+			height: 36px;
+			line-height: 36px;
+		}
+		.td-title {
+			background: #dfe1e4;
+		}
+	}
+	.head {
+		height: 70px;
+		line-height: 70px;
+		width: 100%;
+		background: #000;
+		border-radius: 2px;
+		padding: 0 30px;
+		.title {
+			font-weight: 600;
+			color: rgb(192, 190, 190);
+		}
+		.right-btn {
+			float: right;
+		}
+	}
+	.main-content {
+		.review-content {
+			min-height: 150px;
+			border: 1px solid rgba(192, 190, 190, 0.5);
+			border-top: 0px;
+			padding: 30px;
+			.name {
+				font-weight: 600;
+			}
+			.review-flex {
+				position: relative;
+				width: 100%;
+				margin-top: 15px;
+				div {
+					display: inline-block;
+					width: 24%;
+				}
+			}
+		}
+		.more-height {
+			height: 200px;
+		}
+	}
+}
+</style>
