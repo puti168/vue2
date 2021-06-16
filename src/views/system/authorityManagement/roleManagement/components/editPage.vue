5384c7f12e9d3abc2e5b8d8efe85832b45cc386f
Heviis <abcd1234>
2021-06-16 11:44:06 +0800

    UI

---
+++ src/views/system/authorityManagement/roleManagement/components/editPage.vue
@@ -0,0 +1,288 @@
+<template>
+    <div class="game-container report-container editPicturePage-container">
+        <div class="editPicturePage-content">
+            <div class="form-header">
+                <span>创建/编辑</span>
+                <span>
+					<el-button type="success" @click="back">返回</el-button>
+				</span>
+            </div>
+            <div class="content-part2">
+                <el-form
+                    ref="form"
+                    :model="queryData"
+                    :rules="rules"
+                    label-width="100px"
+                    class="picture-form"
+                >
+                    <el-form-item label="角色名称:" prop="roleName">
+                        <el-input
+                            v-model="queryData.roleName"
+                            size="medium"
+                            maxlength="20"
+                            placeholder="请输入"
+                            clearable
+                            style="width: 365px"
+                        ></el-input>
+                    </el-form-item>
+                    <el-form-item label="备注:" prop="remark">
+                        <el-input
+                            v-model="queryData.remark"
+                            size="medium"
+                            type="textarea"
+                            placeholder="请输入"
+                            clearable
+                            maxlength="50"
+                            show-word-limit
+                            style="width: 365px"
+                        ></el-input>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import list from '@/mixins/list'
+import { isHaveEmoji, notSpecial2 } from '@/utils/validate'
+
+export default {
+    name: 'EditPage',
+    mixins: [list],
+    props: {
+        editData: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    data() {
+        return {
+            loading: false,
+            queryData: {
+                roleName: undefined,
+                remark: undefined,
+                id: undefined
+            },
+            updateStatus: false
+        }
+    },
+    computed: {
+        materialPictureTypeArr() {
+            return this.globalDics.materialPictureType
+        },
+        pictureSizeTypeArr() {
+            return this.globalDics.pictureSizeType
+        },
+        rules() {
+            const testPicName = (rule, value, callback) => {
+                const isSpecial = !notSpecial2(String(value))
+                const isRmoji = isHaveEmoji(String(value))
+                if (isSpecial) {
+                    callback(new Error('不支持空格及特殊字符'))
+                } else if (isRmoji) {
+                    callback(new Error('不支持表情'))
+                } else if (!value) {
+                    callback(new Error('请输入图片名称'))
+                } else {
+                    callback()
+                }
+            }
+
+            return {
+                roleName: [
+                    {
+                        required: true,
+                        validator: testPicName,
+                        trigger: 'blur'
+                    }
+                ],
+                remark: [
+                    {
+                        required: this.updateStatus,
+                        message: '请填入备注',
+                        trigger: 'blur'
+                    },
+                    { min: 2, max: 50, message: '长度在 2 到 50 个字符', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    watch: {
+        editData: {
+            handler(newData) {
+                if (Object.keys(newData).length) {
+                    delete newData.createdAt
+                    delete newData.createdBy
+                    delete newData.updatedAt
+                    delete newData.updatedBy
+                    newData.imageType = newData.imageType + ''
+                    this.queryData = {
+                        ...newData
+                    }
+                    if (this.queryData.imageAddress) {
+                        this.$nextTick(() => {
+                            this.$refs.imgUpload.state = 'image'
+                            this.$refs.imgUpload.fileUrl = newData.imageAddress
+                        })
+                    }
+                    this.updateStatus = true
+                } else {
+                    this.updateStatus = false
+                    this.queryData = {
+                        imageName: undefined,
+                        imageSize: undefined,
+                        imageType: undefined,
+                        displayOrder: undefined,
+                        imageAddress: null,
+                        remark: undefined,
+                        id: undefined
+                    }
+                }
+            },
+            deep: true,
+            immediate: true
+        }
+    },
+    mounted() {
+        // this.$nextTick(() => {
+        // 	!this.updateStatus
+        // 		? (this.queryData.displayOrder = this.lastSortId + 1)
+        // 		: ''
+        // })
+    },
+    updated() {},
+    methods: {
+        back() {
+            this.$emit('back')
+        },
+        add() {
+            this.loading = true
+            const params = {
+                ...this.queryData
+            }
+            const handle = this.updateStatus
+                ? this.$api.agentPictureListUpdateAPI
+                : this.$api.agentPictureListCreateAPI
+            this.$refs['form'].validate((valid) => {
+                if (valid && this.loading) {
+                    handle(params)
+                        .then((res) => {
+                            this.loading = false
+                            const { code, msg } = res
+                            if (code === 200) {
+                                // this.$confirm(`${this.updateStatus ? '更新' : '新增'}成功`, {
+                                // 	confirmButtonText: '确定',
+                                // 	type: 'success',
+                                // 	showCancelButton: false
+                                // })
+                                this.$message({
+                                    message: `${this.updateStatus ? '更新' : '新增'}成功`,
+                                    type: 'success'
+                                })
+                                this.reset()
+                                this.$refs.imgUpload.clearFile()
+                                setTimeout(() => {
+                                    this.back()
+                                }, 1500)
+                            } else {
+                                this.$message({
+                                    message: msg,
+                                    type: 'error'
+                                })
+                            }
+                        })
+                        .catch(() => {
+                            this.loading = false
+                        })
+                }
+            })
+
+            setTimeout(() => {
+                this.loading = false
+            }, 1000)
+        },
+        reset() {
+            this.$refs['form'].resetFields()
+            this.queryData = {
+                imageName: undefined,
+                imageSize: undefined,
+                imageType: undefined,
+                displayOrder: undefined,
+                imageAddress: null,
+                remark: undefined,
+                id: undefined
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/.el-dialog__header {
+    text-align: center;
+    color: #909399;
+    font-weight: 700;
+}
+/deep/ .el-button--info {
+    background-color: #fff;
+    color: rgba(0, 0, 0, 0.64);
+}
+/deep/ .el-button--info:hover {
+    background-color: #eeeded;
+}
+.editPicturePage-container {
+    background-color: #f5f5f5;
+    margin: 0;
+    min-height: calc(100vh - 105px);
+    .editPicturePage-content {
+        width: 50%;
+        margin: 0 auto;
+        background-color: #fff;
+        position: relative;
+        padding-bottom: 30px;
+        .form-header {
+            height: 55px;
+            line-height: 55px;
+            width: 100%;
+            background-color: rgba(0, 0, 0, 1);
+            span:nth-child(1) {
+                position: absolute;
+                left: 30px;
+                top: 0;
+                color: rgba(255, 255, 255, 0.847058823529412);
+                font-size: 16px;
+                font-weight: 700;
+            }
+            span:nth-child(2) {
+                position: absolute;
+                right: 30px;
+                top: 0;
+                color: #999;
+                font-weight: 400;
+                font-size: 14px;
+            }
+        }
+        .form-header:after {
+            content: '.';
+            height: 0;
+            display: table;
+            clear: both;
+        }
+        .content-part2 {
+            width: 100%;
+            padding: 25px 35px 20px;
+            position: relative;
+            margin: 0 auto;
+            .picture-form {
+                position: relative;
+                width: 600px;
+                max-width: 100%;
+                padding: 35px 35px 0;
+                margin: 0 auto;
+                overflow: hidden;
+            }
+        }
+    }
+}
+</style>
--- src/views/system/authorityManagement/roleManagement/index.vue
+++ src/views/system/authorityManagement/roleManagement/index.vue
@@ -1,148 +1,156 @@
 <template>
-	<div class="game-container report-container">
-		<div class="view-container dealer-container">
-			<div class="params">
-				<el-form
-					ref="form"
-					:inline="true"
-					:model="queryData"
-					label-width="100px"
-				>
-					<el-form-item label="角色名称：">
-						<el-input
-							v-model="queryData.roleName"
-							clearable
-							size="medium"
-							style="width: 180px"
-							placeholder="请输入"
-							:disabled="loading"
-							@keyup.enter.native="enterSearch"
-						></el-input>
-					</el-form-item>
-					<el-form-item>
-						<el-button
-							type="primary"
-							icon="el-icon-search"
-							:disabled="loading"
-							size="medium"
-							@click="search"
-						>
-							查询
-						</el-button>
-						<el-button
-							icon="el-icon-refresh-left"
-							:disabled="loading"
-							size="medium"
-							@click="reset"
-						>
-							重置
-						</el-button>
-						<el-button
-							type="warning"
-							icon="el-icon-folder"
-							:disabled="loading"
-							size="medium"
-							style="padding: 0 5px"
-							@click="createRole()"
-						>
-							添加角色
-						</el-button>
-					</el-form-item>
-				</el-form>
-			</div>
-			<div class="content">
-				<el-table
-					v-loading="loading"
-					border
-					size="mini"
-					class="small-size-table"
-					:data="dataList"
-					style="width: 100%"
-					:header-cell-style="getRowClass"
-				>
-					<el-table-column
-						type="index"
-						align="center"
-						label="序号"
-						width="100"
-					></el-table-column>
-					<el-table-column prop="roleName" align="center" label="角色名称">
-						<template slot-scope="scope">
-							<span v-if="!!scope.row.roleName">
-								{{ scope.row.roleName }}
-							</span>
-							<span v-else>-</span>
-						</template>
-					</el-table-column>
-					<el-table-column prop="remark" align="center" label="角色描述">
-						<template slot-scope="scope">
-							<span v-if="!!scope.row.remark">
-								{{ scope.row.remark }}
-							</span>
-							<span v-else>-</span>
-						</template>
-					</el-table-column>
-					<el-table-column prop="createdAt" align="center" label="添加时间">
-						<template slot-scope="scope">
-							<span v-if="!!scope.row.createdAt">
-								{{ scope.row.createdAt }}
-							</span>
-							<span v-else>-</span>
-						</template>
-					</el-table-column>
-					<el-table-column prop="createBy" align="center" label="创建人">
-						<template slot-scope="scope">
-							<span v-if="!!scope.row.createBy">
-								{{ scope.row.createBy }}
-							</span>
-							<span v-else>-</span>
-						</template>
-					</el-table-column>
-					<el-table-column align="center" label="操作">
-						<template slot-scope="scope">
+	<transition name="fade-transform" mode="out-in">
+		<div v-if="!editPage" class="game-container report-container">
+			<div class="view-container dealer-container">
+				<div class="params">
+					<el-form
+						ref="form"
+						:inline="true"
+						:model="queryData"
+						label-width="100px"
+					>
+						<el-form-item label="角色名称：">
+							<el-input
+								v-model="queryData.roleName"
+								clearable
+								size="medium"
+								style="width: 180px"
+								placeholder="请输入"
+								:disabled="loading"
+								@keyup.enter.native="enterSearch"
+							></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-button
+								type="primary"
+								icon="el-icon-search"
+								:disabled="loading"
+								size="medium"
+								@click="search"
+							>
+								查询
+							</el-button>
+							<el-button
+								icon="el-icon-refresh-left"
+								:disabled="loading"
+								size="medium"
+								@click="reset"
+							>
+								重置
+							</el-button>
 							<el-button
 								type="warning"
-								icon="el-icon-edit"
+								icon="el-icon-folder"
+								:disabled="loading"
 								size="medium"
-								@click.stop="editUp(scope.row)"
+								style="padding: 0 5px"
+								@click="openEdit()"
 							>
-								编辑
+								添加角色
 							</el-button>
-							<el-button
-								type="danger"
-								icon="el-icon-delete"
-								size="medium"
-								@click="deleteUp(scope.row)"
-							>
-								删除
-							</el-button>
-						</template>
-					</el-table-column>
-				</el-table>
-				<!-- 分页 -->
-				<el-pagination
-					v-show="!!total"
-					:current-page.sync="pageNum"
-					background
-					layout="total, sizes,prev, pager, next, jumper"
-					:page-size="pageSize"
-					:page-sizes="$store.getters.pageSizes"
-					:total="total"
-					@current-change="handleCurrentChange"
-					@size-change="handleSizeChange"
-				></el-pagination>
+						</el-form-item>
+					</el-form>
+				</div>
+				<div class="content">
+					<el-table
+						v-loading="loading"
+						border
+						size="mini"
+						class="small-size-table"
+						:data="dataList"
+						style="width: 100%"
+						:header-cell-style="getRowClass"
+					>
+						<el-table-column
+							type="index"
+							align="center"
+							label="序号"
+							width="100"
+						></el-table-column>
+						<el-table-column prop="roleName" align="center" label="角色名称">
+							<template slot-scope="scope">
+								<span v-if="!!scope.row.roleName">
+									{{ scope.row.roleName }}
+								</span>
+								<span v-else>-</span>
+							</template>
+						</el-table-column>
+						<el-table-column prop="remark" align="center" label="角色描述">
+							<template slot-scope="scope">
+								<span v-if="!!scope.row.remark">
+									{{ scope.row.remark }}
+								</span>
+								<span v-else>-</span>
+							</template>
+						</el-table-column>
+						<el-table-column prop="createdAt" align="center" label="添加时间">
+							<template slot-scope="scope">
+								<span v-if="!!scope.row.createdAt">
+									{{ scope.row.createdAt }}
+								</span>
+								<span v-else>-</span>
+							</template>
+						</el-table-column>
+						<el-table-column prop="createBy" align="center" label="创建人">
+							<template slot-scope="scope">
+								<span v-if="!!scope.row.createBy">
+									{{ scope.row.createBy }}
+								</span>
+								<span v-else>-</span>
+							</template>
+						</el-table-column>
+						<el-table-column align="center" label="操作">
+							<template slot-scope="scope">
+								<el-button
+									type="warning"
+									icon="el-icon-edit"
+									size="medium"
+									@click.stop="openEdit(scope.row)"
+								>
+									编辑
+								</el-button>
+								<el-button
+									type="danger"
+									icon="el-icon-delete"
+									size="medium"
+									@click="deleteUp(scope.row)"
+								>
+									删除
+								</el-button>
+							</template>
+						</el-table-column>
+					</el-table>
+					<!-- 分页 -->
+					<el-pagination
+						v-show="!!total"
+						:current-page.sync="pageNum"
+						background
+						layout="total, sizes,prev, pager, next, jumper"
+						:page-size="pageSize"
+						:page-sizes="$store.getters.pageSizes"
+						:total="total"
+						@current-change="handleCurrentChange"
+						@size-change="handleSizeChange"
+					></el-pagination>
+				</div>
 			</div>
 		</div>
-	</div>
+        <editPage
+            v-else
+            :rowData="rowData"
+            @back="back"
+        ></editPage>
+	</transition>
 </template>
 
 <script>
 import list from '@/mixins/list'
 import { routerNames } from '@/utils/consts'
+import editPage from './components/editPage'
 
 export default {
 	name: routerNames.roleManage,
-	components: {},
+	components: { editPage },
 	mixins: [list],
 	data() {
 		return {
@@ -151,9 +159,7 @@ export default {
 			},
 			dataList: [],
 			total: 0,
-			showForm: '',
-			editVisible: false,
-			editFormData: {}
+            editPage: false
 		}
 	},
 	computed: {},
@@ -197,17 +203,6 @@ export default {
 			}
 			this.loadData()
 		},
-
-		add() {
-			this.moduleBox = '新增银行信息'
-			this.editVisible = true
-		},
-		submitAdd() {
-			console.log(this.$refs.addForm)
-			//   setAddBank(this.queryData).then((res) => {
-			//     console.log(res);
-			//   });
-		},
 		deleteUp(val) {
 			console.log(val)
 			this.$confirm('确定删除此银行卡号吗?', {
@@ -231,20 +226,16 @@ export default {
 					})
 				})
 		},
-		editUp(val) {
-			this.moduleBox = '修改银行信息'
-			this.editVisible = true
-			this.editFormData = val
-		},
-		submitEdit() {
-			// setEidteBank().then((res) => {
-			//   console.log(res);
-			// });
-		},
-		closeFormDialog() {
-			this.editVisible = false
-		},
-		createRole() {}
+        openEdit(val) {
+            // const { id } = val
+            this.rowData = val
+            // this.rowAssortId = id
+            this.editPage = true
+        },
+        back() {
+            this.editPage = false
+            this.loadData()
+        }
 	}
 }
 </script>
