e4c63d2ae372e2e3462a34a39e7d3edc22dfc513
Calla <976085614@qq.com>
2021-06-25 16:33:57 +0800

    资金联调

--- src/views/funds/withdrawalSettings/memberWithdrawalSettings/index.vue
+++ src/views/funds/withdrawalSettings/memberWithdrawalSettings/index.vue
@@ -239,8 +239,8 @@ width="120px"
               clearable
               :popper-append-to-body="false"
             >
-              <el-option label="开启" :value="0"></el-option>
-              <el-option label="关闭" :value="1"></el-option>
+              <el-option label="开启" :value="1"></el-option>
+              <el-option label="关闭" :value="0"></el-option>
             </el-select>
           </el-form-item>
         </el-form>
