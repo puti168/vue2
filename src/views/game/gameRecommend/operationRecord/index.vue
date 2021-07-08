ea88916876ffc2db31af518d57f0b8ea1cdf415f
Heviis <abcd1234>
2021-07-08 21:16:31 +0800

    fix: 游戏推荐操作记录排序取消状态修改

--- src/views/game/gameRecommend/operationRecord/index.vue
+++ src/views/game/gameRecommend/operationRecord/index.vue
@@ -195,7 +195,6 @@ export default {
         // 降序
         this.queryData.orderType = 'desc'
       } else {
-        delete this.queryData.orderKey
         delete this.queryData.orderType
       }
       this.loadData()
