91aa0c9cf3ab2bc3ade0f1180724bdc3fd32851e
Heviis <abcd1234>
2021-07-15 20:56:06 +0800

    feat: 首页推荐位游戏专题整理删除逻辑

--- src/views/game/gameRecommend/gameHomeRecommend/editPage/components/game.vue
+++ src/views/game/gameRecommend/gameHomeRecommend/editPage/components/game.vue
@@ -209,6 +209,7 @@ export default {
 				moduleDesc: undefined
 			},
 			dataList: [],
+            idArray: [],
 			gameAssortDicList: []
 		}
 	},
@@ -230,15 +231,15 @@ export default {
 		}
 	},
 	created() {
+		this.getDetails()
+	},
+	mounted() {
 		// 拖动排序
 		document.body.ondrop = function(event) {
 			event.preventDefault()
 			event.stopPropagation()
 		}
 		this.columnDrop()
-		this.getDetails()
-	},
-	mounted() {
 		this.gameAssortList()
 	},
 	methods: {
@@ -249,15 +250,34 @@ export default {
 			e.target.value = e.target.value.replace(/[^\d]/g, '')
 		},
 		getDetails() {
+            this.dataList = []
+            this.loading = true
 			this.$api.gameSpecialDetailsAPI().then((res) => {
-				const { code, data } = res
+                this.loading = false
+				const {
+					code,
+					data: { gameTopicModuleMetaVos },
+                    msg
+				} = res
 				if (code === 200) {
-					this.dataList = data['gameTopicModuleMetaVos']
-						? data['gameTopicModuleMetaVos']
-						: []
-				}
+					this.dataList = gameTopicModuleMetaVos || []
+                    this.idArray =
+                        gameTopicModuleMetaVos &&
+                        gameTopicModuleMetaVos.length &&
+                        gameTopicModuleMetaVos.map((item) => item.id)
+				} else {
+                    this.loading = false
+                    this.$message({
+                        message: msg,
+                        type: 'error'
+                    })
+                }
 				console.log('res', res)
-			})
+			}).catch(() => (this.loading = false))
+
+            setTimeout(() => {
+                this.loading = false
+            }, 1500)
 		},
 		gameAssortList() {
 			this.$api.gameAssortDicAPI().then((res) => {
@@ -276,36 +296,60 @@ export default {
 			// }
 		},
 		confirm() {},
-		deleteRow(row) {
-			console.log(row)
-			this.$confirm('确定删除此游戏吗?', {
-				confirmButtonText: '确定',
-				cancelButtonText: '取消',
-				type: 'warning'
-			})
-				.then(() => {
-					// const loading = this.$loading({
-					// 	lock: true,
-					// 	text: 'Loading',
-					// 	spinner: 'el-icon-loading',
-					// 	background: 'rgba(0, 0, 0, 0.7)'
-					// })
-					// this.$api
-					// 	.setDeleteRole('', val.id)
-					// 	.then((res) => {
-					// 		loading.close()
-					// 		this.$message({
-					// 			type: 'success',
-					// 			message: '删除成功!'
-					// 		})
-					// 		this.loadData()
-					// 	})
-					// 	.catch(() => {
-					// 		loading.close()
-					// 	})
-				})
-				.catch(() => {})
-		},
+        deleteRow(val) {
+            const { id } = val
+            this.$confirm('确定删除此游戏吗?', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            })
+                .then(() => {
+                    const loading = this.$loading({
+                        lock: true,
+                        text: 'Loading',
+                        spinner: 'el-icon-loading',
+                        background: 'rgba(0, 0, 0, 0.7)'
+                    })
+                    if (this.idArray.includes(id)) {
+                        this.$api
+                            .gameDelTopicModuleAPI({ id })
+                            .then((res) => {
+                                loading.close()
+                                const { code } = res
+                                if (code === 200) {
+                                    this.$message({
+                                        type: 'success',
+                                        message: '删除成功!'
+                                    })
+                                } else {
+                                    this.$message({
+                                        type: 'error',
+                                        message: '删除失败!'
+                                    })
+                                }
+                                this.loadData()
+                            })
+                            .catch(() => {
+                                loading.close()
+                                this.$message({
+                                    type: 'error',
+                                    message: '删除失败!'
+                                })
+                            })
+                    } else {
+                        this.dataList = this.dataList.filter((item) => {
+                            return item.id !== id
+                        })
+                        // this.updateArr = this.updateArr.filter((item) => {
+                        // 	return item.id !== id
+                        // })
+                    }
+                    setTimeout(() => {
+                        loading.close()
+                    }, 1000)
+                })
+                .catch(() => {})
+        },
 		add() {
 			this.loading = true
 			const params = {
@@ -364,17 +408,25 @@ export default {
 
 		// 列拖动
 		columnDrop() {
-			console.log('旧数据', this.dataList)
 			const wrapperTr = document.querySelector('.el-table__body-wrapper tbody')
-			const _this = this
-			this.sortable = Sortable.create(wrapperTr, {
-				animation: 180,
-				delay: 0,
-				onEnd: ({ newIndex, oldIndex }) => {
-					const currRow = _this.dataList.splice(oldIndex, 1)[0]
-					_this.dataList.splice(newIndex, 0, currRow)
-				}
-			})
+            const _this = this
+            this.sortable =
+                wrapperTr &&
+                Sortable.create(wrapperTr, {
+                    animation: 300,
+                    delay: 0,
+                    onEnd: ({ newIndex, oldIndex }) => {
+                        console.log('newIndex', newIndex)
+                        console.log('oldIndex', oldIndex)
+                        const currRow = _this.dataList.splice(oldIndex, 1)[0]
+                        _this.dataList.splice(newIndex, 0, currRow)
+                        if (newIndex !== oldIndex) {
+                            _this.dataList.forEach((item, idx) => {
+                                item.displayOrder = idx + 1
+                            })
+                        }
+                    }
+                })
 		},
 		reset() {
 			this.$refs['form'].resetFields()
