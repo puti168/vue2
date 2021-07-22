a03d9b28ded40c7f71f6ae31d605f8e365ebed8c
Heviis <abcd1234>
2021-07-22 23:16:11 +0800

    feat: 优化多选框

--- src/views/operation/activityConfig/activityConfigLog/index.vue
+++ src/views/operation/activityConfig/activityConfigLog/index.vue
@@ -616,6 +616,7 @@
 								v-model="queryData.changeType"
 								style="width: 300px"
 								multiple
+                                collapse-tags
 								placeholder="默认选择全部"
 								:popper-append-to-body="false"
 							>
