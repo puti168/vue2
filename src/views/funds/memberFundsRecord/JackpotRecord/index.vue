9ea0381c5ad284d034129b819cd851e8c6a93eaa
darcy <darcy.com>
2021-07-26 15:36:56 +0800

    需求变更

--- src/views/funds/memberFundsRecord/JackpotRecord/index.vue
+++ src/views/funds/memberFundsRecord/JackpotRecord/index.vue
@@ -3,9 +3,25 @@
     <div class="view-container dealer-container">
       <div class="params">
         <el-form ref="form" :inline="true" :model="queryData">
-          <el-form-item label="派彩时间:">
+          <el-form-item label="领取时间:">
             <el-date-picker
-              v-model="searchTime"
+              v-model="receiveTime"
+              size="medium"
+              :picker-options="pickerOptions"
+              format="yyyy-MM-dd HH:mm:ss"
+              type="datetimerange"
+              range-separator="-"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              align="right"
+              :clearable="false"
+              :default-time="defaultTime"
+              style="width: 375px"
+            ></el-date-picker>
+          </el-form-item>
+          <el-form-item label="领取时间:">
+            <el-date-picker
+              v-model="orderTime"
               size="medium"
               :picker-options="pickerOptions"
               format="yyyy-MM-dd HH:mm:ss"
@@ -29,6 +45,21 @@
               :disabled="loading"
               @keyup.enter.native="enterSearch"
             ></el-input>
+          </el-form-item>
+          <el-form-item label="订单状态：" class="tagheight">
+            <el-select
+              v-model="queryData.auditStatus"
+              clearable
+              placeholder="默认选择全部"
+              :popper-append-to-body="false"
+            >
+              <el-option
+                v-for="item in activityPayoutStatus"
+                :key="item.code"
+                :label="item.description"
+                :value="item.code"
+              ></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item label="会员账号:">
             <el-input
@@ -54,15 +85,45 @@
               @keyup.enter.native="enterSearch"
             ></el-input>
           </el-form-item>
-          <el-form-item label="订单状态：" class="tagheight">
+          <el-form-item label="领取方式:" class="tagheight">
             <el-select
-              v-model="queryData.auditStatus"
+              v-model="queryData.approveType"
               clearable
               placeholder="默认选择全部"
               :popper-append-to-body="false"
             >
               <el-option
-                v-for="item in activityPayoutStatus"
+                v-for="item in activityApproveType"
+                :key="item.code"
+                :label="item.description"
+                :value="item.code"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="领取状态:" class="tagheight">
+            <el-select
+              v-model="queryData.approveType"
+              clearable
+              placeholder="默认选择全部"
+              :popper-append-to-body="false"
+            >
+              <el-option
+                v-for="item in activityApproveType"
+                :key="item.code"
+                :label="item.description"
+                :value="item.code"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="活动类型:" class="tagheight">
+            <el-select
+              v-model="queryData.approveType"
+              clearable
+              placeholder="默认选择全部"
+              :popper-append-to-body="false"
+            >
+              <el-option
+                v-for="item in activityApproveType"
                 :key="item.code"
                 :label="item.description"
                 :value="item.code"
@@ -81,21 +142,7 @@
               @keyup.enter.native="enterSearch"
             ></el-input>
           </el-form-item>
-          <el-form-item label="派彩方式:" class="tagheight">
-            <el-select
-              v-model="queryData.approveType"
-              clearable
-              placeholder="默认选择全部"
-              :popper-append-to-body="false"
-            >
-              <el-option
-                v-for="item in activityApproveType"
-                :key="item.code"
-                :label="item.description"
-                :value="item.code"
-              ></el-option>
-            </el-select>
-          </el-form-item>
+
           <el-form-item>
             <el-button
               type="primary"
@@ -115,7 +162,7 @@
               重置
             </el-button>
             <el-button
-			  v-if="hasPermission('267')"
+              v-if="hasPermission('267')"
               icon="el-icon-download"
               type="warning"
               :disabled="loading"
@@ -164,7 +211,12 @@
               <span v-else>-</span>
             </template>
           </el-table-column>
-          <el-table-column prop="auditStatus" align="center" label="订单状态">
+          <el-table-column prop="auditStatus" align="center" label="领取方式">
+            <template slot-scope="scope">
+              {{ typeFilter(scope.row.auditStatus, "activityPayoutStatus") }}
+            </template>
+          </el-table-column>
+          <el-table-column prop="auditStatus" align="center" label="领取状态">
             <template slot-scope="scope">
               {{ typeFilter(scope.row.auditStatus, "activityPayoutStatus") }}
             </template>
@@ -184,7 +236,7 @@
           <el-table-column
             prop="amount"
             align="center"
-            label="彩金金额"
+            label="优惠金额"
             width="200px"
             sortable="custom"
           >
@@ -192,7 +244,14 @@
           <el-table-column
             prop="createdAt"
             align="center"
-            label="派彩时间"
+            label="领取时间"
+            sortable="custom"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="createdAt"
+            align="center"
+            label="订单生成时间"
             sortable="custom"
           >
           </el-table-column>
@@ -217,265 +276,259 @@
 <script>
 import list from '@/mixins/list'
 import dayjs from 'dayjs'
-const startTime = dayjs()
-	.startOf('day')
-	.valueOf()
-const endTime = dayjs()
-	.endOf('day')
-	.valueOf()
+const startTime = dayjs().startOf('day').valueOf()
+const endTime = dayjs().endOf('day').valueOf()
 
 export default {
-	components: {},
-	mixins: [list],
-	data() {
-		return {
-			queryData: {},
-			searchTime: [startTime, endTime],
-			tableData: [],
-			summary: {}
-		}
-	},
-	computed: {
-		activityPayoutStatus() {
-			return this.globalDics.activityPayoutStatus
-		},
-		activityApproveType() {
-			return this.globalDics.activityApproveType
-		}
-	},
-	created() {},
-	methods: {
-		loadData() {
-			this.loading = true
-			const create = this.searchTime || []
-			const [startTime, endTime] = create
-			let params = {
-				...this.queryData,
-				createdAtStart: startTime
-					? dayjs(startTime).format('YYYY-MM-DD HH:mm:ss')
-					: '',
-				createdAtEnd: endTime
-					? dayjs(endTime).format('YYYY-MM-DD HH:mm:ss')
-					: ''
-			}
-			params = {
-				...this.getParams(params)
-			}
-			this.$api
-				.getMemberFundsRecordsDiscount(params)
-				.then((res) => {
-					if (res.code === 200) {
-						this.tableData = res.data.record
-						this.total = res.data.totalRecord
-						this.summary = res.data.summary
-					}
-					this.loading = false
-				})
-				.catch(() => {
-					this.loading = false
-				})
-		},
-		reset() {
-			this.queryData = {}
-			this.searchTime = [startTime, endTime]
-			this.pageNum = 1
-			this.loadData()
-		},
-		_changeTableSort({ column, prop, order }) {
-			if (prop === 'amount') {
-				prop = 1
-			}
-			if (prop === 'createdAt') {
-				prop = 2
-			}
-			this.pageNum = 1
-			this.queryData.orderKey = prop
-			if (order === 'ascending') {
-				// 升序
-				this.queryData.orderType = 'asc'
-			} else if (order === 'descending') {
-				// 降序
-				this.queryData.orderType = 'desc'
-			} else {
-				delete this.queryData.orderKey
-				delete this.queryData.orderType
-			}
-			this.loadData()
-		},
-		exportExcel() {
-			this.loading = true
-			const create = this.searchTime || []
-			const [startTime, endTime] = create
-			let params = {
-				...this.queryData,
-				createdAtStart: startTime
-					? dayjs(startTime).format('YYYY-MM-DD HH:mm:ss')
-					: '',
-				createdAtEnd: endTime
-					? dayjs(endTime).format('YYYY-MM-DD HH:mm:ss')
-					: ''
-			}
-			params = {
-				...this.getParams(params)
-			}
-			this.$confirm(
-				`<strong>是否导出?</strong></br><span style='font-size:12px;color:#c1c1c1'>数据过大时，请耐心等待</span>`,
-				'确认提示',
-				{
-					dangerouslyUseHTMLString: true,
-					confirmButtonText: '确定',
-					cancelButtonText: '取消',
-					type: 'warning'
-				}
-			)
-				.then(() => {
-					this.$api
-						.getMemberFundsRecordsDiscountDownload(params)
-						.then((res) => {
-							this.loading = false
-							const { data, status } = res
-							if (res && status === 200) {
-								const { type } = data
-								if (type.includes('application/json')) {
-									const reader = new FileReader()
-									reader.onload = (evt) => {
-										if (evt.target.readyState === 2) {
-											const {
-												target: { result }
-											} = evt
-											const ret = JSON.parse(result)
-											if (ret.code !== 200) {
-												this.$message({
-													type: 'error',
-													message: ret.msg,
-													duration: 1500
-												})
-											}
-										}
-									}
-									reader.readAsText(data)
-								} else {
-									const result = res.data
-									const disposition = res.headers['content-disposition']
-									const fileNames = disposition && disposition.split("''")
-									let fileName = fileNames[1]
-									fileName = decodeURIComponent(fileName)
-									const blob = new Blob([result], {
-										type: 'application/octet-stream'
-									})
-									if ('download' in document.createElement('a')) {
-										const downloadLink = document.createElement('a')
-										downloadLink.download = fileName || ''
-										downloadLink.style.display = 'none'
-										downloadLink.href = URL.createObjectURL(blob)
-										document.body.appendChild(downloadLink)
-										downloadLink.click()
-										URL.revokeObjectURL(downloadLink.href)
-										document.body.removeChild(downloadLink)
-									} else {
-										window.navigator.msSaveBlob(blob, fileName)
-									}
-									this.$message({
-										type: 'success',
-										message: '导出成功',
-										duration: 1500
-									})
-								}
-							}
-						})
-						.catch(() => {
-							this.loading = false
-							// this.$message({
-							//   type: "error",
-							//   message: "导出失败",
-							//   duration: 1500,
-							// });
-						})
-				})
-				.catch(() => {})
-		},
-		getSummaries(param) {
-			const { columns } = param
-			const sums = []
-			columns.forEach((column, index) => {
-				if (index === 0) {
-					const el = (
-						<div class='count_row'>
-							<p>小计</p>
-							<p>合计</p>
-						</div>
-					)
-					sums[index] = el
-					return
-				} else if (index === 7 && this.summary !== null) {
-					const el = (
-						<div class='count_row'>
-							<p>{this.summary.subtotal}</p>
-							<p>{this.summary.total}</p>
-						</div>
-					)
-					sums[index] = el
-					return
-				} else {
-					sums[index] = (
-						<div class='count_row'>
-							<p />
-							<p />
-						</div>
-					)
-				}
-			})
+  components: {},
+  mixins: [list],
+  data() {
+    return {
+      queryData: {},
+      receiveTime: [startTime, endTime],
+      orderTime: [startTime, endTime],
+      tableData: [],
+      summary: {}
+    }
+  },
+  computed: {
+    activityPayoutStatus() {
+      return this.globalDics.activityPayoutStatus
+    },
+    activityApproveType() {
+      return this.globalDics.activityApproveType
+    }
+  },
+  created() {},
+  methods: {
+    loadData() {
+      this.loading = true
+      const receive = this.receiveTime || []
+      const [startTime, endTime] = receive
+    //   const order = this.orderTime || []
+    //   const [start, end] = order
+      let params = {
+        ...this.queryData,
+        createdAtStart: startTime ? dayjs(startTime).format('YYYY-MM-DD HH:mm:ss') : '',
+        createdAtEnd: endTime ? dayjs(endTime).format('YYYY-MM-DD HH:mm:ss') : ''
+      }
+      params = {
+        ...this.getParams(params)
+      }
+      this.$api
+        .getMemberFundsRecordsDiscount(params)
+        .then((res) => {
+          if (res.code === 200) {
+            this.tableData = res.data.record
+            this.total = res.data.totalRecord
+            this.summary = res.data.summary
+          }
+          this.loading = false
+        })
+        .catch(() => {
+          this.loading = false
+        })
+    },
+    reset() {
+      this.queryData = {}
+      this.receiveTime = [startTime, endTime]
+      this.orderTime = [startTime, endTime]
+      this.pageNum = 1
+      this.loadData()
+    },
+    _changeTableSort({ column, prop, order }) {
+      if (prop === 'amount') {
+        prop = 1
+      }
+      if (prop === 'createdAt') {
+        prop = 2
+      }
+      this.pageNum = 1
+      this.queryData.orderKey = prop
+      if (order === 'ascending') {
+        // 升序
+        this.queryData.orderType = 'asc'
+      } else if (order === 'descending') {
+        // 降序
+        this.queryData.orderType = 'desc'
+      } else {
+        delete this.queryData.orderKey
+        delete this.queryData.orderType
+      }
+      this.loadData()
+    },
+    exportExcel() {
+      this.loading = true
+      const receive = this.receiveTime || []
+      const [startTime, endTime] = receive
+    //   const order = this.orderTime || []
+    //   const [start, end] = order
+      let params = {
+        ...this.queryData,
+        createdAtStart: startTime ? dayjs(startTime).format('YYYY-MM-DD HH:mm:ss') : '',
+        createdAtEnd: endTime ? dayjs(endTime).format('YYYY-MM-DD HH:mm:ss') : ''
+      }
+      params = {
+        ...this.getParams(params)
+      }
+      this.$confirm(
+        `<strong>是否导出?</strong></br><span style='font-size:12px;color:#c1c1c1'>数据过大时，请耐心等待</span>`,
+        '确认提示',
+        {
+          dangerouslyUseHTMLString: true,
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      )
+        .then(() => {
+          this.$api
+            .getMemberFundsRecordsDiscountDownload(params)
+            .then((res) => {
+              this.loading = false
+              const { data, status } = res
+              if (res && status === 200) {
+                const { type } = data
+                if (type.includes('application/json')) {
+                  const reader = new FileReader()
+                  reader.onload = (evt) => {
+                    if (evt.target.readyState === 2) {
+                      const {
+                        target: { result }
+                      } = evt
+                      const ret = JSON.parse(result)
+                      if (ret.code !== 200) {
+                        this.$message({
+                          type: 'error',
+                          message: ret.msg,
+                          duration: 1500
+                        })
+                      }
+                    }
+                  }
+                  reader.readAsText(data)
+                } else {
+                  const result = res.data
+                  const disposition = res.headers['content-disposition']
+                  const fileNames = disposition && disposition.split("''")
+                  let fileName = fileNames[1]
+                  fileName = decodeURIComponent(fileName)
+                  const blob = new Blob([result], {
+                    type: 'application/octet-stream'
+                  })
+                  if ('download' in document.createElement('a')) {
+                    const downloadLink = document.createElement('a')
+                    downloadLink.download = fileName || ''
+                    downloadLink.style.display = 'none'
+                    downloadLink.href = URL.createObjectURL(blob)
+                    document.body.appendChild(downloadLink)
+                    downloadLink.click()
+                    URL.revokeObjectURL(downloadLink.href)
+                    document.body.removeChild(downloadLink)
+                  } else {
+                    window.navigator.msSaveBlob(blob, fileName)
+                  }
+                  this.$message({
+                    type: 'success',
+                    message: '导出成功',
+                    duration: 1500
+                  })
+                }
+              }
+            })
+            .catch(() => {
+              this.loading = false
+              // this.$message({
+              //   type: "error",
+              //   message: "导出失败",
+              //   duration: 1500,
+              // });
+            })
+        })
+        .catch(() => {})
+    },
+    getSummaries(param) {
+      const { columns } = param
+      const sums = []
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          const el = (
+            <div class='count_row'>
+              <p>小计</p>
+              <p>合计</p>
+            </div>
+          )
+          sums[index] = el
+          return
+        } else if (index === 7 && this.summary !== null) {
+          const el = (
+            <div class='count_row'>
+              <p>{this.summary.subtotal}</p>
+              <p>{this.summary.total}</p>
+            </div>
+          )
+          sums[index] = el
+          return
+        } else {
+          sums[index] = (
+            <div class='count_row'>
+              <p />
+              <p />
+            </div>
+          )
+        }
+      })
 
-			return sums
-		},
-		enterSearch() {
-			this.loadData()
-		}
-	}
+      return sums
+    },
+    enterSearch() {
+      this.loadData()
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 /deep/ .el-table__footer-wrapper .cell::after {
-	border: 1px solid #ebeef5;
-	content: '';
-	position: absolute;
-	top: 41px;
-	left: 0;
-	width: 100%;
+  border: 1px solid #ebeef5;
+  content: "";
+  position: absolute;
+  top: 41px;
+  left: 0;
+  width: 100%;
 }
 
 /deep/ .el-table__fixed-footer-wrapper tr::after {
-	border: 1px solid #ebeef5;
-	content: '';
-	position: absolute;
-	top: 41px;
-	left: 0;
-	width: 100%;
+  border: 1px solid #ebeef5;
+  content: "";
+  position: absolute;
+  top: 41px;
+  left: 0;
+  width: 100%;
 }
 .count_row {
-	height: 80px;
-	p {
-		height: 40px;
-		line-height: 40px;
-		color: #5c5c5c;
-		font-weight: 700;
-		span {
-			display: inline-block;
-			width: 20px;
-			height: 20px;
-		}
-	}
+  height: 80px;
+  p {
+    height: 40px;
+    line-height: 40px;
+    color: #5c5c5c;
+    font-weight: 700;
+    span {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+    }
+  }
 }
 /deep/.el-table {
-	overflow: auto;
+  overflow: auto;
 }
 /deep/.el-table__body-wrapper,
 /deep/.el-table__header-wrapper,
 /deep/.el-table__footer-wrapper {
-	overflow: visible;
+  overflow: visible;
 }
 /deep/.el-table::after {
-	position: relative !important;
+  position: relative !important;
 }
 </style>
