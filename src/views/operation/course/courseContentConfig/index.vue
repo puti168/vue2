5e667a8a7b764e7be4b48ff407d07a9554d0f2db
Calla <976085614@qq.com>
2021-08-13 11:34:48 +0800

    feat: 教程内容描述增加输入字符

--- src/views/operation/course/courseContentConfig/index.vue
+++ src/views/operation/course/courseContentConfig/index.vue
@@ -329,7 +329,7 @@
 							<el-input
 								v-model="dialogForm.contentDesc"
 								type="textarea"
-								:maxlength="100"
+								:maxlength="300"
 								autocomplete="off"
 								style="width: 500px"
 								placeholder="请输入"
